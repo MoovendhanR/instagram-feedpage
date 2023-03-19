b0ddebca1851aff6438fdba8d9419c7578c7eddd
MoovendhanR <moovendhanr1011@gmail.com>
2023-03-19 17:52:50 +0530

    table part done

--- src/Components/UiCreation.jsx
+++ src/Components/UiCreation.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import { PieChart, Pie} from 'recharts';
 const UiCreation = () => {
     const data = [
-        {name: 'Geeksforgeeks', students: 400},
-        {name: 'Technical scripter', students: 700},
-        {name: 'Geek-i-knack', students: 200},
         {name: 'Geek-o-mania', students: 1000}
       ];
         
@@ -30,15 +27,51 @@ const UiCreation = () => {
         </div>
         <div className="inner-cont3">
               <div>
-                  <PieChart width={300} height={300}>
-                  <Pie data={data} dataKey="students" outerRadius={250} fill="green" />
+                <p>Submited Activite's v/s Pending Submissions</p>
+                  <PieChart width={250} height={200} cx="50%" cy="50%">
+                  <Pie data={data} dataKey="students" outerRadius={500} fill="green"cx="50%" cy="50%" />
                   </PieChart>
+                  <p style={{ color: 'green'}}>⬛Submitted<span style={{ color: 'blue'}}>⬛Pending Submissions</span></p>
               </div>
-              <div>
-                   
+              <div>     
               </div>
         </div>
-        <div></div>
+        <br />
+        <div className="inner-cont4">
+             <h3>Active Students</h3>
+             <table>
+        <tr>
+          <th>Name</th>
+          <th>Last Activity</th>
+          <th>Sessions</th>
+          <th>Words</th>
+          <th>Submission Date</th>
+          <th>Type ID</th>
+          <th>ID</th>
+          
+        </tr>
+        <tr>
+          <td>Traceveg 653</td>
+          <td>12 hours ago</td>
+          <td>7</td>
+          <td>65</td>
+          <td>12 hours ago</td>
+          <td>100%</td>
+          <td>100%</td>
+        
+        </tr>
+        <tr>
+        <td>Will Kinsman</td>
+          <td>a month ago</td>
+          <td>2</td>
+          <td>118</td>
+          <td>a month ago</td>
+          <td>80%</td>
+          <td>44%</td>
+        </tr>
+       
+      </table>
+        </div>
         <div></div>
        </div>
     </>
